a22cd1ed839f94a16cffeaefbd5c97715b023a00
PatrickWang-1 <35417456+PatrickWang-1@users.noreply.github.com>
2024-03-21 21:47:34 +0800

    Add files via upload

--- selectionSort.c
+++ selectionSort.c
@@ -2,9 +2,8 @@
 #include<math.h>
 #include<stdlib.h>
 #include<time.h>
-void quickSort(int arr[],int left,int right);
+void selectionSort(int arr[],int arrLen);
 void swap(int* a,int* b);
-int sort(int arr[],int l,int r);
 int main(){
     clock_t start,end;
     int i,arrLen_a,arrLen_b,arrLen_c,arrLen_d,arrLen_e;
@@ -36,7 +35,7 @@ int main(){
     }
     printf("\n");
     start=clock();
-    quickSort(a,0,arrLen_a-1);
+    selectionSort(a,arrLen_a);
     end=clock();
     double time_used_a=((double)end-start)/CLOCKS_PER_SEC;
     for (i=0;i<arrLen_a;i++)
@@ -46,56 +45,42 @@ int main(){
     printf("\ntime_used_10: %lf",time_used_a);
     
     start=clock();
-    quickSort(b,0,arrLen_b-1);
+    selectionSort(b,arrLen_b);
     end=clock();
     double time_used_b=((double)end-start)/CLOCKS_PER_SEC;
     printf("\ntime_used_100: %lf",time_used_b);
 
     start=clock();
-    quickSort(c,0,arrLen_c-1);
+    selectionSort(c,arrLen_c);
     end=clock();
     double time_used_c=((double)end-start)/CLOCKS_PER_SEC;
     printf("\ntime_used_1000: %lf",time_used_c);
 
     start=clock();
-    quickSort(d,0,arrLen_d-1);
+    selectionSort(d,arrLen_d);
     end=clock();
     double time_used_d=((double)end-start)/CLOCKS_PER_SEC;
     printf("\ntime_used_10000: %lf",time_used_d);
 
     start=clock();
-    quickSort(e,0,arrLen_e-1);
+    selectionSort(e,arrLen_e);
     end=clock();
     double time_used_e=((double)end-start)/CLOCKS_PER_SEC;
     printf("\ntime_used_100000: %lf",time_used_e);
     return 0;
 }
+void selectionSort(int arr[],int arrLen){
+    int i,j;
+    for (i=0;i<arrLen;i++){
+        for (j=i+1;j<arrLen;j++){
+            if (arr[i]>arr[j]){
+                swap(&arr[i],&arr[j]);
+            }
+        }
+    }
+}
 void swap(int* a,int* b){
-    int temp;
-    temp=*a;
+    int temp=*a;
     *a=*b;
     *b=temp;
-}
-void quickSort(int arr[],int left,int right){
-    if (left>=right){
-        return;
-    }
-    int stop=sort(arr,left,right);
-    quickSort(arr,left,stop-1);
-    quickSort(arr,stop+1,right);
-}
-int sort(int arr[],int l,int r){
-    int i=l;
-    int j=r;
-    int pivot=l;
-    while (i<j) {
-        while (i<j&&arr[j]>=arr[pivot])
-            j--;
-        while (i<j&&arr[i]<=arr[pivot])
-            i++;
-        swap(&arr[j], &arr[i]);
-    }
-    int stop=i;
-    swap(&arr[stop],&arr[pivot]);
-    return stop;
 }
